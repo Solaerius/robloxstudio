5e00da9574a6804fa63b194af3685267a6b4be01
solaerius <eddie@ervenius.com>
2026-06-16 21:54:01 +0200

    add config

---
+++ config.js
@@ -0,0 +1,7 @@
+// Local dev config -- replace with real values before deploying.
+export const APPWRITE_ENDPOINT = 'https://fra.cloud.appwrite.io/v1';
+export const APPWRITE_PROJECT_ID = '6a31a19900214aaad1e8';
+export const APPWRITE_DATABASE_ID = 'portfolio';
+export const COMMISSION_COLLECTION_ID = 'commission_status';
+export const CONTACT_COLLECTION_ID = 'contact_submissions';
+export const PROJECTS_COLLECTION_ID = 'projects';
